5f5987e89f6cba48999ce4b0173fc0cca3d62182
zzEnds <qianfei.com@163.com>
2016-09-06 15:42:10 +0800

    --1C success

--- src/com/lu/wang/C1.java
+++ src/com/lu/wang/C1.java
@@ -14,115 +14,119 @@ public class C1 {
 	public static DecimalFormat df = new DecimalFormat("0.000000");
 	public static void main(String[] args) {
 		
+//		Long timea = System.currentTimeMillis();//time for
+			
 		Scanner in=new Scanner(System.in);
 		double[][] num = new double[3][2]; 
 		
-//		num[0][0] = in.nextFloat();
-//		num[0][1] = in.nextFloat();
-//		num[1][0] = in.nextFloat();
-//		num[1][1] = in.nextFloat();
-//		num[2][0] = in.nextFloat();
-//		num[2][1] = in.nextFloat();
+		//submit
+		num[0][0] = in.nextDouble();
+		num[0][1] = in.nextDouble();
+		num[1][0] = in.nextDouble();
+		num[1][1] = in.nextDouble();
+		num[2][0] = in.nextDouble();
+		num[2][1] = in.nextDouble();	
 		
-		num[0][0] = 17.288379;
-		num[0][1] = 68.223317;
-		num[1][0] = 48.776683;
-		num[1][1] = 71.688379;
-		num[2][0] = 23.170559;
-		num[2][1] = 106.572762;
+		//test1
+//		num[0][0] = 0;
+//		num[0][1] = 0;
+//		num[1][0] = 1;
+//		num[1][1] = 1;
+//		num[2][0] = 0;
+//		num[2][1] = 1;
 		
-//		getArea(num);
+		//test3
+//		num[0][0] = 18.716839;
+//		num[0][1] = 40.852752;
+//		num[1][0] = 66.147248;
+//		num[1][1] = -4.083161;
+//		num[2][0] = 111.083161;
+//		num[2][1] = 43.347248;
+		
+		//test6
+//		num[0][0] = 17.288379;
+//		num[0][1] = 68.223317;
+//		num[1][0] = 48.776683;
+//		num[1][1] = 71.688379;
+//		num[2][0] = 23.170559;
+//		num[2][1] = 106.572762;
+		
+		//test9
+//		num[0][0] = 34.236058;
+//		num[0][1] = 108.163949;
+//		num[1][0] = 28.639345;
+//		num[1][1] = 104.566515;
+//		num[2][0] = 25.610069;
+//		num[2][1] = 86.002927;
+		
 		String re = df.format(getArea(num));
 		System.out.println(re);
+		
+//		System.out.println(System.currentTimeMillis()-timea);//time for
 	}
 	
 	public static double getArea(double[][] num) {
 		
-		double[] tmp = getDistance(num);
-		double X = tmp[0]; 
-//		double X = getShortest(tmp);
-		double angelX = getAngel(tmp, 0);
-		double angel1 = getAngel(tmp, 1);
-		double angel2 = getAngel(tmp, 2);
+		double A = Math.pow((num[0][0]-num[1][0]),2) + Math.pow((num[0][1]-num[1][1]),2);
+		double B = Math.pow((num[1][0]-num[2][0]),2) + Math.pow((num[1][1]-num[2][1]),2);
+		double C = Math.pow((num[0][0]-num[2][0]),2) + Math.pow((num[0][1]-num[2][1]),2);
+		double a = Math.sqrt(A);
+		double b = Math.sqrt(B);
+		double c = Math.sqrt(C);
 		
-		//n=?
-		double propotion1 = angelX / angel1;
-		double propotion2 = angelX / angel2;
+		double angelC = Math.acos((A + B - C)/(2*a*b));
+		double angelA = Math.acos((C + B - A)/(2*c*b));
+		double angelB = Math.PI - angelA - angelC;
 		
-		double part1 = Math.PI / angelX;
-		double part2 = X * X / 4;
-		double part3 = 1 / Math.tan(angelX);
-		
-		return part1 * part2 * part3;
-	}
-	
-	public static double[] getDistance(double[][] num) {
-		
-		double[] tmp = new double[3];
-		
-		double a = Math.pow((num[0][0]-num[1][0]),2) + Math.pow((num[0][1]-num[1][1]),2);
-		double b = Math.pow((num[1][0]-num[2][0]),2) + Math.pow((num[1][1]-num[2][1]),2);
-		double c = Math.pow((num[0][0]-num[2][0]),2) + Math.pow((num[0][1]-num[2][1]),2);
-		a = Math.sqrt(a);
-		b = Math.sqrt(b);
-		c = Math.sqrt(c);
+		double XX;
+		double angelX;
 		
 		if(a>b) {
 			if(b>c) {
-				tmp[0] = c;
-				tmp[1] = a;
-				tmp[2] = b;
+				angelX = angelC;
+				XX = C;
 			} else {
-				tmp[0] = b;
-				tmp[1] = a;
-				tmp[2] = c;
+				angelX = angelB;
+				XX = B;
+				angelB = angelC;
 			}
 		} else {
 			if(a>c) {
-				tmp[0] = c;
-				tmp[1] = a;
-				tmp[2] = b;
+				angelX = angelC;
+				XX = C;
 			} else {
-				tmp[0] = a;
-				tmp[1] = b;
-				tmp[2] = c;
+				angelX = angelA;
+				XX = A;
+				angelA = angelC;
 			}
 		}
 		
-		return tmp;
-	}
-	
-//	public static double getShortest(double[] num) {
-//		if(num[0]>num[1]) {
-//			if(num[1]>num[2]) {
-//				return num[2];
-//			} else {
-//				return num[1];
-//			}
-//		} else {
-//			if(num[0]>num[2]) {
-//				return num[2];
-//			} else {
-//				return num[0];
-//			}
-//		}
-//	}
-	
-	public static double getAngel(double[] num, int i) {
-		//(b^2 + c^2 - X^2)/2bc
-		double arccos = 0;
-		if(i == 0) {
-			arccos = (Math.pow(num[1],2) + Math.pow(num[2],2) - Math.pow(num[0],2))/(2*num[1]*num[2]);
-		} else if(i == 1) {
-			arccos = (Math.pow(num[0],2) + Math.pow(num[2],2) - Math.pow(num[1],2))/(2*num[0]*num[2]);
-		} else {
-			arccos = (Math.pow(num[1],2) + Math.pow(num[0],2) - Math.pow(num[2],2))/(2*num[1]*num[0]);
-		}
+		//n=?
+		double judge1, judge2 = 0;
+		int param = 0;
+		do{
+			param++;
+			judge1 = angelA * param / angelX;
+			if(!ofint(judge1)) continue;
+			judge2 = angelB * param / angelX;
+		}while (!ofint(judge1) || !ofint(judge2));
 		
-//		double angel = Math.acos(arccos) * 180 / Math.PI;
-		double angel = Math.acos(arccos);
-		return angel;
+//		int n = (int) (param * Math.PI / angelX);
+//		double alphaa = angelX / param;
+		double part1 = param * Math.PI * XX * 0.125 / angelX;
+		double part2 = Math.sin(2 * angelX / param) / Math.pow(Math.sin(angelX), 2);
+		double areaS = part1 * part2;
+		
+		
+		return areaS;
 	}
 	
+	public static boolean ofint(double x) {
+		int xx = (int)Math.round(x);
+		double judge = Math.abs(x-xx);
+		if(judge < 0.0001) {
+			return true;
+		} else return false;
+	}
 
 }
