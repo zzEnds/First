2adb768442f864028f11c366575c34ff40f5e6ef
zzEnds <qianfei.com@163.com>
2016-12-02 18:07:04 +0800

    Signed-off-by: zzEnds <qianfei.com@163.com>

--- src/com/lu/wang/getCodeforces/GetDoneInfo.java
+++ src/com/lu/wang/getCodeforces/GetDoneInfo.java
@@ -1,14 +1,15 @@
 package com.lu.wang.getCodeforces;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-
-
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.management.InstanceAlreadyExistsException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,19 +18,38 @@ import org.jsoup.nodes.Element;
 
 public class GetDoneInfo {
 	
+	final static String ACCEPTS = "acceptIds";
 	final static String CODEFORCE = "http://codeforces.com/";
 	final static int MAX_PAGE = 1;
 	final static String DIV_CLASS = "datatable";
 	final static String TAG_TR = "tr";
 	final static String TAG_TD = "td";
+	final static String TAG_A = "a";
 	
 	public static void main(String[] args) {
 		
-		getCntOfPeople();
+		List<String> ids = getAcceptIds();
+		getCntOfPeople(ids);
 		
 	}
 	
-	public static void getCntOfPeople() {
+	public static List<String> getAcceptIds() {
+		String fileName = ACCEPTS;
+		List<String> idList = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+			String line;
+			while((line = br.readLine()) != null) {
+				idList.add(line.trim());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			return idList;
+		}
+	}
+	
+	public static void getCntOfPeople(List<String> ids) {
 		
 		for (int pageNo = 1; pageNo <= MAX_PAGE; pageNo++) {
 			
@@ -49,9 +69,14 @@ public class GetDoneInfo {
 					Document doc = Jsoup.parse(wholePage.toString());
 					Element div = doc.getElementsByClass(DIV_CLASS).get(0);
 					List<Element> trs = div.getElementsByTag(TAG_TR);
-//					while() {
-//						
-//					}
+					for (int i = 0; i < trs.size(); i++) {
+						if(!isEmpty(trs.get(i)) && 
+								!isEmpty(trs.get(i).getElementsByTag(TAG_TD)) && 
+								!isEmpty(trs.get(i).getElementsByTag(TAG_TD).get(0).getElementsByTag(TAG_A))) {
+							String title = trs.get(i).getElementsByTag(TAG_TD).get(0).getElementsByTag(TAG_A).get(0).data();
+							System.out.println(title);
+						}
+					}
 //					doc.getClass();
 				}
 				
@@ -59,6 +84,16 @@ public class GetDoneInfo {
 				// TODO: handle exception
 			}
 		}
+	}
+	
+	public static boolean isEmpty(Object ob) {
+		if(ob.getClass().isInstance(List.class)) {
+			List x = (List)ob;
+			if(null == x || 0 >= x.size()) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public static void getWebInfo(String urlStr, String elementName) {
